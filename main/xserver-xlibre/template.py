0eaeede4cd2a8f40b77ff97bb424a23a3772532b
matu6968 <wierzejskimateusz8@gmail.com>
2025-11-21 23:15:57 +0000

    This is a automated commit for dependency upgrades, messages are below: main/xserver-xlibre: upgrade to version xlibre-xserver-25.0.0.16

--- main/xserver-xlibre/template.py
+++ main/xserver-xlibre/template.py
@@ -1,5 +1,5 @@
 pkgname = "xserver-xlibre"
-pkgver = "25.0.0.15"
+pkgver = "25.0.0.16"
 pkgrel = 0
 build_style = "meson"
 _fontroot = "/usr/share/fonts"
@@ -95,7 +95,7 @@ pkgdesc = "Xlibre X server"
 license = "MIT AND BSD-3-Clause"
 url = "https://github.com/X11Libre/xserver"
 source = f"https://github.com/X11Libre/xserver/archive/refs/tags/xlibre-xserver-{pkgver}.tar.gz"
-sha256 = "cebbe482328a3dc04b8d74e0ca81f849a7ef1a3dd46275f55f3218fb5d60e5f3"
+sha256 = "0187e57331daefa75ddc33a4f878ef4c08526b83dc91dbc51a25375d2ff33b8c"
 tool_flags = {
     "CFLAGS": ["-D_GNU_SOURCE", "-D__uid_t=uid_t", "-D__gid_t=gid_t"],
     "LDFLAGS": ["-Wl,-z,lazy"],  # must be set for modules to work
