747f048206b19bf1e033bcb135d6b9c0e888d64b
matu6968git <wierzejskimateusz8@gmail.com>
2026-01-10 15:37:54 +0100

    main/xserver-xlibre: upgrade to version xlibre-xserver-25.1.1

--- main/xserver-xlibre/template.py
+++ main/xserver-xlibre/template.py
@@ -1,5 +1,5 @@
 pkgname = "xserver-xlibre"
-pkgver = "25.1.0"
+pkgver = "25.1.1"
 pkgrel = 0
 build_style = "meson"
 _fontroot = "/usr/share/fonts"
@@ -97,7 +97,7 @@ pkgdesc = "Xlibre X server"
 license = "MIT AND BSD-3-Clause"
 url = "https://github.com/X11Libre/xserver"
 source = f"https://github.com/X11Libre/xserver/archive/refs/tags/xlibre-xserver-{pkgver}.tar.gz"
-sha256 = "b46fe85e306821504a5e0e91aa0d8d54ce7ddfb67106c495a6ca5ac2c3e28f06"
+sha256 = "f1d0ca035758716941786618a230d6ff440655734235c2d427d13cdb410f1beb"
 tool_flags = {
     "CFLAGS": ["-D_GNU_SOURCE", "-D__uid_t=uid_t", "-D__gid_t=gid_t"],
     "LDFLAGS": ["-Wl,-z,lazy"],  # must be set for modules to work
